d562f6caa86d73c5dc7a2396133c35f2040b1b3f
aebishkek <aebishkek@gmail.com>
2017-04-15 17:23:37 -0400

    Odd Numbers

--- src/com/cbt/AppTest.java
+++ src/com/cbt/AppTest.java
@@ -3,7 +3,7 @@ package com.cbt;
 public class AppTest {
 	public static void main(String[] args) {
 		for(int num=0; num<=100; num++ ){
-			if(num%2==0){
+			if(num%2!=0){
 				System.out.print(num+" ");
 			}
 		}
