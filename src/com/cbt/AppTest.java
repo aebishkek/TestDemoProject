01bf5e5942e92af3548acf7e7cbe2d5c8d5e20f9
aebishkek <aebishkek@gmail.com>
2017-04-15 17:03:16 -0400

    Even Numbers

--- src/com/cbt/AppTest.java
+++ src/com/cbt/AppTest.java
@@ -2,6 +2,10 @@ package com.cbt;
 
 public class AppTest {
 	public static void main(String[] args) {
-		
+		for(int num=0; num<=100; num++ ){
+			if(num%2==0){
+				System.out.print(num+" ");
+			}
+		}
 	}
 }
